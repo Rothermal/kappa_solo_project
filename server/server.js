b03760c7502c636fabf9e30e838d3d8aa0c04ea3
Mark Rothermal <rothermal2@comcast.net>
2016-04-06 12:01:31 -0500

    added procfile for heroku, started building server file

--- server/server.js
+++ server/server.js
@@ -1,3 +1,13 @@
 /**
  * Created by JFCS on 4/6/16.
  */
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+
+
+
+var server = app.listen(port,function(){
+   var port = server.address().port;
+    console.log('now open on port',port);
+});
