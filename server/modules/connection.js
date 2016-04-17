21b13cbb457d634feeb986dbf8d75d5fde47076b
Mark Rothermal <rothermal2@comcast.net>
2016-04-16 19:36:35 -0500

    working on getting heroku up

--- server/modules/connection.js
+++ server/modules/connection.js
@@ -1,10 +1,20 @@
 // connection.js
-var connectionString = '';
+var connectionString =  process.env.DATABASE_URL || 'postgres://localhost:5432/my_side_jobs_db';
 
-if(process.env.DATABASE_URL != undefined) {
-    connectionString = process.env.DATABASE_URL + 'ssl';
-} else {
-    connectionString = 'postgres://localhost:5432/my_side_jobs_db';
+//if(process.env.DATABASE_URL != undefined) {
+//    connectionString = process.env.DATABASE_URL + 'ssl';
+//} else {
+//    connectionString = 'postgres://localhost:5432/my_side_jobs_db';
+//}
+
+
+if(connectionString = process.env.DATABASE_URL) {
+    pg.defaults.ssl = true;
 }
 
+
+console.log('connectionstring in connection js',connectionString);
 module.exports = connectionString;
+
+
+
